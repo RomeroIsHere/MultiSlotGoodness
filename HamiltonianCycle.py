7c3c7fe6f2483a96e80706f1aefcfc640dd66474
Hayumin <guillermoromecu@gmail.com>
2026-03-06 23:17:41 -0600

    Commented Every function For Future reference

--- HamiltonianCycle.py
+++ HamiltonianCycle.py
@@ -1,3 +1,6 @@
+# Imports
+from typing import Any, Iterable
+# because i want Type hints
 class Graph(): 
     def __init__(self, vertices:int): 
         # Makes an Epty Adjacency Matrix of the Correct Size
@@ -5,49 +8,75 @@ class Graph():
                                     for row in range(vertices)] 
         self.vertices_count = vertices 
 
-    def is_safe_to_add(self, v, pos, path): 
-        if self.adjacency_matrix[path[pos-1]][v] == 0: 
+    def is_safe_to_add(self, path, pos, candidateVertex): 
+        '''Check if the Candidate Vertex is a valid Vertex to add to the path
+        
+        a) The candidate vertex is adjacent to the latest path vertex
+        
+        and 
+
+        b) The candidate Vertex is not included in the Path Taken to get here
+        '''
+        if not self.is_adjacent(path[pos-1], candidateVertex):
             return False
 
         for vertex in path: 
-            if vertex == v: 
+            if vertex == candidateVertex: 
                 return False
 
         return True
+    
+    def is_adjacent(self, vertexA:int, vertexB:int):
+        '''Checks if 2 Nodes are Adjacent, using the Adjacency Matrix
+        
+        This check is Directional, Meaning that It returns true so long as Vertex A can go to Vertex B, but the Reverse is not Necessarily true
+        '''
+        if self.adjacency_matrix[vertexA][vertexB] == 1: 
+            return True
+        else: 
+            return False
 
-    def hamiltonian_cycle_util(self, path, pos): 
-        if pos == self.vertices_count: 
-            if self.adjacency_matrix[path[pos-1]][path[0]] == 1: 
-                return True
-            else: 
-                return False
+    def hamiltonian_cycle_util(self, path, recurse_depth): 
+        '''Recursive function to check Hamiltonian cycle
+        Recursion stops when Recursion Depth (recurse_depth) is Equal to the number of Vertices in the Graph, then:
+            Returns True if the Final vertex is adjacent to first
+        '''
+        # Check if You've traversed all nodes by checking Depth to the Total Number of Vertices
+        if recurse_depth == self.vertices_count:
+            return self.is_adjacent(path[recurse_depth-1], path[0])
 
         for v in range(1, self.vertices_count): 
-            if self.is_safe_to_add(v, pos, path): 
-                path[pos] = v 
+            if self.is_safe_to_add(path, recurse_depth, v): 
 
-                if self.hamiltonian_cycle_util(path, pos+1): 
+                path[recurse_depth] = v 
+
+                if self.hamiltonian_cycle_util(path, recurse_depth+1): 
+                    # if this path found a valid Cycle, shortcircuit and don't check any more configurations
                     return True
+                # if it did not, means that the path we have so far is Wrong, and we must go back to a Previous step to continue Searching
 
-                path[pos] = -1
-
+                # Reset the Vertex added at this step
+                path[recurse_depth] = -1
+        # if the search never Short circuits, then there must be no Path that Satisfies the 
         return False
 
     def find_hamiltonian_cycle(self):
+        '''Brute Forces a Hamiltonian Cycle of the Graph using Backtracking'''
         # Makes a List representing the path (Exactly the Size of the Number of Vertices)
         path = [-1] * self.vertices_count 
         # Arbitraly Start it at 0 (The cycle can Start Anywhere because it is a Cycle, so the Starting Point is Irrelevant in the Cycle case)
         path[0] = 0
-
+        
         if not self.hamiltonian_cycle_util(path, 1): 
             print ("No\n")
             return False
-
+        
+        print ("Yes\n")
         self.print_solution(path) 
         return True
 
-    def print_solution(self, path): 
-        print ("Yes\n")
+    def print_solution(self, path:Iterable[Any]):
+        '''Arbitrarily prints the elements of an iterable(list, set, tuple, etc...)'''
         for vertex in path: 
             print (vertex )
 
