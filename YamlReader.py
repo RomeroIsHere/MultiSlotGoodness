9dd8a0ef340c2e049c7c501fd128c68932ec0c6a
Hayumin <guillermoromecu@gmail.com>
2026-03-12 00:58:30 -0600

    Make Worlds and Players REadable from YAML

--- YamlReader.py
+++ YamlReader.py
@@ -1,9 +1,19 @@
 import yaml
-
+from Players import Player
+from Worlds import World
 
 with open("DSG-data.yaml") as stream:
     try:
-        print(yaml.safe_load(stream))
+        yamlObject=yaml.safe_load(stream)
+        print(yamlObject)
+        PlayerList=set()
+        for Slot in yamlObject.get('SlotName'):
+            PlayerList.add(Player(Slot))
+        WorldList=set()
+        for AvaibleWorlds in yamlObject.get('Worlds'):
+            WorldList.add(World(AvaibleWorlds))
 
+        print(PlayerList)
+        print(WorldList)
     except yaml.YAMLError as exc:
         print(exc)
