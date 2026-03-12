294ede7102c0cf725e6e524de1503099fbc255a9
Hayumin <guillermoromecu@gmail.com>
2026-03-12 01:16:50 -0600

    Add World to Every Player Able to Play said World

--- YamlReader.py
+++ YamlReader.py
@@ -5,14 +5,14 @@ from Worlds import World
 with open("DSG-data.yaml") as stream:
     try:
         yamlObject=yaml.safe_load(stream)
-        print(yamlObject)
-        PlayerList=set()
+        PlayerList=dict()
         for Slot in yamlObject.get('SlotName'):
-            PlayerList.add(Player(Slot))
-        WorldList=set()
+            PlayerList[Slot]=(Player(Slot))
+        WorldList=dict()
         for AvaibleWorlds in yamlObject.get('Worlds'):
-            WorldList.add(World(AvaibleWorlds))
-
+            WorldList[AvaibleWorlds]=(yamlObject.get(AvaibleWorlds))
+            for Slot in WorldList[AvaibleWorlds]:
+                PlayerList[Slot].acceptable_worlds.add(AvaibleWorlds)
         print(PlayerList)
         print(WorldList)
     except yaml.YAMLError as exc:
