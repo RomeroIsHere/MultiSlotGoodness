14ece0e7e0b4fed64f81fc75c3e5580e1852ce66
Hayumin <guillermoromecu@gmail.com>
2026-03-12 01:25:21 -0600

    Added Id for indexing into Matrix

--- Players.py
+++ Players.py
@@ -6,30 +6,32 @@ class Player:
 
     keep in mind, for every player, there will be 2 slots created
     '''
-    def __init__(self,player_name):
+    def __init__(self,player_name, index):
         # Given Slot Name of the Player
         self.name=player_name
+        self.id=index
         # Worlds that they Can play
         # Currently random For Dev Purposes
         self.acceptable_worlds=set()
         #TODO add a way to initialize this from a File or Something
     def __str__(self) -> str:
-        return self.name + ': ' + repr(self.acceptable_worlds)
+        return str(self.id)+".-"+self.name + ': ' + repr(self.acceptable_worlds)
     def __repr__(self) -> str:
         return self.__str__()
     def printWorlds(self):
-        print(self.name,self.acceptable_worlds)
+        print(self.name,self.id,self.acceptable_worlds)
 
 
 if __name__ == "__main__":
     testing_var_global_world=['alpha','beta','charlie','delta']
     testing_names=['alice','bob','cassandra','daryl','esther','frank']
     playerList=[]
-    for slot in testing_names:
-        currentPlayer=Player(slot)
+    for id, slot in enumerate(testing_names):
+        currentPlayer=Player(slot,id)
         # for x in range(random.randrange(len(testing_var_global_world))):
         currentPlayer.acceptable_worlds=set(random.choices(testing_var_global_world,k=3)) 
         # has a Chance to pick the same twice, but since it turns into set it does not  duplicate the World name
         playerList.append(currentPlayer)
     for player in playerList:
         player.printWorlds()
+        print(player)
--- YamlReader.py
+++ YamlReader.py
@@ -6,8 +6,9 @@ with open("DSG-data.yaml") as stream:
     try:
         yamlObject=yaml.safe_load(stream)
         PlayerList=dict()
-        for Slot in yamlObject.get('SlotName'):
-            PlayerList[Slot]=(Player(Slot))
+        PlayerSlotsName =yamlObject.get('SlotName')
+        for id, Slot in enumerate(PlayerSlotsName):
+            PlayerList[Slot]=(Player(Slot, id))
         WorldList=dict()
         for AvaibleWorlds in yamlObject.get('Worlds'):
             WorldList[AvaibleWorlds]=(yamlObject.get(AvaibleWorlds))
