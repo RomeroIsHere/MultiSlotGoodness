a9a42f54a90ab3ea2779e968daf52c9fbd52272f
Guillermo Romero Cuevas <guillermoromecu@gmail.com>
2026-03-27 19:10:44 -0600

    useful: Added Debug Prints to Parser

--- YamlReader.py
+++ YamlReader.py
@@ -1,11 +1,12 @@
 import yaml
 from Players import Player
 
-def ParseYaml() -> dict[str,Player]:
+def ParseDSGYaml() -> dict[str,Player]:
+    print("Parsing YAML")
     with open("DSG-data.yaml") as stream:
         try:
             yamlObject=yaml.safe_load(stream)
-            TempPlayerDict=dict()
+            TempPlayerDict=dict() # [str,Player]
             PlayerSlotsName =yamlObject.get('SlotName')
             # Add a Player with an ID to use as an index
             for id, Slot in enumerate(PlayerSlotsName):
@@ -13,16 +14,26 @@ def ParseYaml() -> dict[str,Player]:
             
             WorldList=yamlObject.get('Worlds')
             for WorldName in WorldList.keys():
+                print("World:",WorldName)
                 for Slot in WorldList[WorldName]:
-                    TempPlayerDict[Slot].acceptable_worlds.add(WorldName)
-                    for Partner in WorldList[WorldName]:
-                        TempPlayerDict[Slot].AddToCompatible(TempPlayerDict[Partner])
+                    if not (Slot in PlayerSlotsName):
+                         print("Player not in List:",Slot)
+                    else:
+                        print("    Player:",Slot)
+                        TempPlayerDict[Slot].acceptable_worlds.add(WorldName)
+                        for Partner in WorldList[WorldName]:
+                            if (Partner in PlayerSlotsName):
+                                TempPlayerDict[Slot].AddToCompatible(TempPlayerDict[Partner])
+                            else:
+                                print("Player not in List:",Partner)
+            
             return TempPlayerDict
         except yaml.YAMLError as exc:
             print(exc)
         return dict()
 if __name__ == "__main__":
     DictOfPlayers=dict()
-    print(ParseYaml())
-    
+    for PlayerName,PlayerObj in ParseDSGYaml().items():
+                if isinstance(PlayerObj, Player):
+                    print(PlayerName,":",len(PlayerObj.CompatiblePlayers))
     
