734fd510ea1c33db5141b87261dcf53af7f7a6f3
Hayumin <hayumigumi@gmail.com>
2026-03-28 18:55:29 -0600

    Added a Tool to test Avergae Compatibility of the Paths generated

---
+++ TestGen.py
@@ -0,0 +1,20 @@
+from Main import Graph
+import YamlReader
+
+if __name__ == "__main__":
+    SlotNamesAdjacencyDict=dict()
+    PlayerDict=YamlReader.ParseDSGYaml()
+    for Slot in PlayerDict.keys():
+        SlotNamesAdjacencyDict[Slot]=PlayerDict[Slot].CompatiblePlayers
+    AvgCumulativeCompatibility=0
+    iterationsCount=30
+    for iteration in range(iterationsCount):
+        HamiltonTraveler=Graph(SlotNamesAdjacencyDict)
+        HamiltonTraveler.find_hamiltonian_cycle()
+        CumulativeCompatibilityForIteration=0
+        for cur, nxt in zip (HamiltonTraveler.finishedPath, HamiltonTraveler.finishedPath [1:] + [ HamiltonTraveler.finishedPath[0]] ):
+            ListOfCompatibility = PlayerDict[HamiltonTraveler.random_order_vertex_list[cur]].acceptable_worlds & PlayerDict[HamiltonTraveler.random_order_vertex_list[nxt]].acceptable_worlds
+            CumulativeCompatibilityForIteration+=len(ListOfCompatibility)
+        AvgCumulativeCompatibility+=CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)
+        print(f"Average Compatibility for iteration #{iteration}:{CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)}")
+    print(f"Average of {iterationsCount} Generations:{AvgCumulativeCompatibility/iterationsCount}")
