b47100f57b0be8ab51662c94f9394323b3775e00
Hayumin <hayumigumi@gmail.com>
2026-03-28 18:32:47 -0600

    Option to Switch From Printing World and Players On/off

--- YamlReader.py
+++ YamlReader.py
@@ -1,31 +1,37 @@
 import yaml
 from Players import Player
 
-def ParseDSGYaml() -> dict[str,Player]:
+def ParseDSGYaml(shouldPrint=False) -> dict[str,Player]:
     print("Parsing YAML")
     with open("DSG-data.yaml") as stream:
         try:
             yamlObject=yaml.safe_load(stream)
             TempPlayerDict=dict() # [str,Player]
             PlayerSlotsName =yamlObject.get('SlotName')
+            if shouldPrint:
+                print("Number Of Players:",len(PlayerSlotsName))
             # Add a Player with an ID to use as an index
             for id, Slot in enumerate(PlayerSlotsName):
                 TempPlayerDict[Slot]=(Player(Slot, id))
             
             WorldList=yamlObject.get('Worlds')
             for WorldName in WorldList.keys():
-                print("World:",WorldName)
+                if shouldPrint:
+                    print("World:",WorldName)
                 for Slot in WorldList[WorldName]:
                     if not (Slot in PlayerSlotsName):
-                         print("Player not in List:",Slot)
+                        if shouldPrint:
+                            print("Player not in List:",Slot)
                     else:
-                        print("    Player:",Slot)
+                        if shouldPrint:
+                            print("    Player:",Slot)
                         TempPlayerDict[Slot].acceptable_worlds.add(WorldName)
                         for Partner in WorldList[WorldName]:
                             if (Partner in PlayerSlotsName):
                                 TempPlayerDict[Slot].AddToCompatible(TempPlayerDict[Partner])
                             else:
-                                print("Player not in List:",Partner)
+                                if shouldPrint:
+                                    print("Player not in List:",Partner)
             
             return TempPlayerDict
         except yaml.YAMLError as exc:
@@ -33,7 +39,7 @@ def ParseDSGYaml() -> dict[str,Player]:
         return dict()
 if __name__ == "__main__":
     DictOfPlayers=dict()
-    for PlayerName,PlayerObj in ParseDSGYaml().items():
+    for PlayerName,PlayerObj in ParseDSGYaml(True).items():
                 if isinstance(PlayerObj, Player):
                     print(PlayerName,":",len(PlayerObj.CompatiblePlayers))
     
