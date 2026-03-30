76fbb3b6731d9fd8860816dc3918a919a9b1ea6c
Hayumin <hayumigumi@gmail.com>
2026-03-29 20:00:36 -0600

    Changed from Fixed iteration to continuos Testing Until Keyboard interrupt

--- TestGen.py
+++ TestGen.py
@@ -1,7 +1,6 @@
 from Main import Graph
 import YamlReader
-
-if __name__ == "__main__":
+def mainTest():
     SlotNamesAdjacencyDict=dict()
     PlayerDict=YamlReader.ParseDSGYaml()
     for Slot in PlayerDict.keys():
@@ -18,3 +17,32 @@ if __name__ == "__main__":
         AvgCumulativeCompatibility+=CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)
         print(f"Average Compatibility for iteration #{iteration}:{CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)}")
     print(f"Average of {iterationsCount} Generations:{AvgCumulativeCompatibility/iterationsCount}")
+
+
+
+if __name__ == "__main__":
+    SlotNamesAdjacencyDict=dict()
+    PlayerDict=YamlReader.ParseDSGYaml()
+    for Slot in PlayerDict.keys():
+        SlotNamesAdjacencyDict[Slot]=PlayerDict[Slot].CompatiblePlayers
+    AvgCumulativeCompatibility=0
+    iterationsCount=0
+    try:
+        while True:
+            HamiltonTraveler=Graph(SlotNamesAdjacencyDict)
+            HamiltonTraveler.find_hamiltonian_cycle()
+            CumulativeCompatibilityForIteration=0
+            for cur, nxt in zip (HamiltonTraveler.finishedPath, HamiltonTraveler.finishedPath [1:] + [ HamiltonTraveler.finishedPath[0]] ):
+                ListOfCompatibility = PlayerDict[HamiltonTraveler.random_order_vertex_list[cur]].acceptable_worlds & PlayerDict[HamiltonTraveler.random_order_vertex_list[nxt]].acceptable_worlds
+                CumulativeCompatibilityForIteration+=len(ListOfCompatibility)
+            AvgCumulativeCompatibility+=CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)
+            print(f"Average Compatibility for iteration #{iterationsCount}:{CumulativeCompatibilityForIteration/len(HamiltonTraveler.finishedPath)}")
+            iterationsCount+=1
+            pass
+            
+    except KeyboardInterrupt:
+        pass
+    if iterationsCount:
+        print(f"Average of {iterationsCount} Generations:{AvgCumulativeCompatibility/iterationsCount}")
+    else:
+        print("Found Nothing")
