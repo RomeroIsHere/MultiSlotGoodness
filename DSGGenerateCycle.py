131deab073c44154f718ce8a88be44dc2346669b
RomeroIsHere <145238541+RomeroIsHere@users.noreply.github.com>
2026-04-09 03:04:00 -0600

    Create Terminal Menu (#5)
    * Adds Menu Making And Handling Functionality
    
    * Rename To Import Compatible Names
    
    * Make Callable Mains on Import
    
    * Added a Main Menu
    
    ---------
    
    Co-authored-by: Hayumin <hayumigumi@gmail.com>

--- DSGGenerateCycle.py
+++ DSGGenerateCycle.py
@@ -4,10 +4,8 @@ from Models import Graph
 
 logger=logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    Utility.MakeDir("logs")
-    logging.basicConfig(filename='logs/main.log', encoding='utf-8', level=logging.INFO, format='[%(asctime)s]%(name)s:%(levelname)s %(message)s',  datefmt='%I:%M:%S')
-    logger.info("Starting Application")
+def GenerateCycle():
+    logger.info("Starting Generation")
     SlotNamesAdjacencyDict=dict()
     PlayerDict=InputHandler.ParseDSGYaml()
     logger.info("Succesfully got PlayerDict")
@@ -23,3 +21,9 @@ if __name__ == "__main__":
         OutputFile = OutputHandler.WriteYAMLOutFile(HamiltonTraveler, PlayerDict)
         logger.info(f"Wrote Cycle as YAML to {OutputFile}")
         OutputHandler.WriteCSVFile(OutputFile, OutputFile + '.dsv')
+
+if __name__ == "__main__":
+    Utility.MakeDir("logs")
+    logging.basicConfig(filename='logs/generate.log', encoding='utf-8', level=logging.INFO, format='[%(asctime)s]%(name)s:%(levelname)s %(message)s',  datefmt='%I:%M:%S')
+    GenerateCycle()
+    
--- DSGRenamePlayerFiles.py
+++ DSGRenamePlayerFiles.py
@@ -2,8 +2,10 @@ import logging
 from DSGFileHandler import InputHandler, OutputHandler, Utility
 logger=logging.getLogger(__name__)
 
+def RenameYamls():
+    renamedict=InputHandler.ParseRenameYaml()
+    OutputHandler.RenameAndCopyYAML(renamedict)
 if __name__ == "__main__":
     Utility.MakeDir("logs")
     logging.basicConfig(filename='logs/renaming.log', encoding='utf-8', level=logging.INFO, format='[%(asctime)s]%(name)s:%(levelname)s %(message)s',  datefmt='%I:%M:%S')
-    renamedict=InputHandler.ParseRenameYaml()
-    OutputHandler.RenameAndCopyYAML(renamedict)
+    RenameYamls()
