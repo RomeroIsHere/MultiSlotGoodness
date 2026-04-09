b6e92c50b8705af33c0eef388001305df5db06f2
Hayumin <hayumigumi@gmail.com>
2026-04-09 03:01:52 -0600

    Added a Main Menu

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+from Menu import Menu
+from Models import Player
+from DSGFileHandler import OutputHandler, InputHandler, Utility
+import DSGTestAverageCompatibility, DSGGenerateCycle, DSGRenamePlayerFiles
+
+def RenameYamls():
+    renamedict=InputHandler.ParseRenameYaml()
+    OutputHandler.RenameAndCopyYAML(renamedict)
+
+if __name__ == "__main__":
+    Utility.MakeDir("logs")
+    logging.basicConfig(filename='logs/main.log', encoding='utf-8', level=logging.INFO, format='[%(asctime)s]%(name)s:%(levelname)s %(message)s',  datefmt='%I:%M:%S')
+    listOfActions=[
+        ('Check Number of Compatible Partner', InputHandler.CompatiblePlayers),
+        ('Generate Cycle', DSGGenerateCycle.GenerateCycle),
+        ('Rename Collected Yamls', DSGRenamePlayerFiles.RenameYamls),
+        ('Test Average Game Compatibility of Cycles', DSGTestAverageCompatibility.mainTest)
+    ]
+    Menu.menu("Double Slot Goodness Tool",listOfActions)
