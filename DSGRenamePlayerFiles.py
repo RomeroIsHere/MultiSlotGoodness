512605091c47712c522cf3f4af3175a718b3bd20
Hayumin <hayumigumi@gmail.com>
2026-04-16 15:55:24 -0600

    Adds Rename logs for Management Use

--- DSGRenamePlayerFiles.py
+++ DSGRenamePlayerFiles.py
@@ -3,7 +3,25 @@ from DSGFileHandler import InputHandler, OutputHandler, Utility
 logger=logging.getLogger(__name__)
 
 def RenameYamls():
-    renamedict=InputHandler.ParseRenameYaml()
+    renamedict: dict[str,str|int]=InputHandler.ParseRenameYaml()
+    EveryNameSet=set()
+    RepeatedSender=set()
+    RepeatedReceiver=set()
+    for Sender, Receiver in renamedict.items():
+        if Sender in EveryNameSet:
+            RepeatedSender.add(Sender)
+        EveryNameSet.add(Sender)
+        if Receiver in EveryNameSet:
+            RepeatedReceiver.add(Receiver)
+        EveryNameSet.add(Receiver)
+    if len(RepeatedReceiver) or len(RepeatedSender):
+        logger.error(f"Names Repeat Within the Duplications")
+        for Element in sorted(RepeatedReceiver):
+            print(Element)
+            logger.warning(f'Receiving Player "{Element}" is already present in the set')
+        for Element in sorted(RepeatedSender):
+            print(Element)
+            logger.warning(f'Sending Player "{Element}" is already present in the set')
     OutputHandler.RenameAndCopyYAML(renamedict)
 if __name__ == "__main__":
     Utility.MakeDir("logs")
