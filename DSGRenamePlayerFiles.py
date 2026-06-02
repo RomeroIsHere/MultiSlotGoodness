735dc1b2e22b17af4a0725f8f69362423fbe4e50
Hayumin <hayumigumi@gmail.com>
2026-06-02 11:40:07 -0600

    Refactor: Separate into It's Own File

--- DSGRenamePlayerFiles.py
+++ DSGRenamePlayerFiles.py
@@ -24,10 +24,8 @@ def RenameYamls():
             logger.warning(f'Sending Player "{Element}" is already present in the set')
     OutputHandler.RenameAndCopyYAML(renamedict)
 
-def StripPlando():
-    OutputHandler.RenameAndStripYAML()
 
 if __name__ == "__main__":
     Utility.MakeDir("logs")
     logging.basicConfig(filename='logs/renaming.log', encoding='utf-8', level=logging.INFO, format='[%(asctime)s]%(name)s:%(levelname)s %(message)s',  datefmt='%I:%M:%S')
-    StripPlando()
+    RenameYamls()
